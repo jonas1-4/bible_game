52253c14a37a9a6350313b7a38c6cc8371894349
Jonas <jonas1.4@gmx.de>
2021-07-09 02:42:37 +0200

    fixed json setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 
+import 'dart:convert';
+
+import 'package:bible_game/services/jsons.dart';
 import 'package:flutter/material.dart';
 
 import 'activities/menus/homescreen.dart';
 
 const String HomescreenPath = '/homescreen';
 const String VerseselectPath = '/verseselect';
+String bible = '';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  bible = await JsonService().getJson('assets/de_schlachter.json');
 
-void main() {
+  final data = jsonDecode(bible);
+  print('${data[0]['chapters'][0][0]}');
   runApp(MaterialApp(
     initialRoute: HomescreenPath,
     routes: {
--- lib/services/jsons.dart
+++ lib/services/jsons.dart
@@ -2,7 +2,7 @@ import 'package:flutter/services.dart';
 
 class JsonService {
   ///getJson() Takes an Path to an Json and Returns it as an Future<String>
-  Future<String> getJson(String jsonPath) {
-  return rootBundle.loadString('jsonPath');
-}
+  Future<String> getJson(String jsonPath) async {
+    return rootBundle.loadString(jsonPath);
+  }
 }
